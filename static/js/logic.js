52d49d77b1275fb5688fa9b09c8bd3d7b8a51e51
Beverly Thompson <blt1339@gmail.com>
2021-01-22 10:49:48 -0500

    Add leaflet into index.html and add function to loop through and output data from csv files.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,30 @@
+// Loop through the lending borrowing data groups
+// collect all of the categories in one array and
+// and all of the year data and then output for 
+// whatever data is passed
+function outputData(data) {
+  // console.log(data.columns);
+  let categoryColumn = data.columns[0];
+
+  for (var i = 0; i < data.length; i++) {
+    yearData = {};
+    let category = data[i][categoryColumn]
+
+    // rowData["category"] = category;
+
+    if (category !== "" && category !== "©IMF, 2020") {
+      console.log(category);
+      for (yearIndex = 1990; yearIndex < 2026; yearIndex++) {
+        // console.log(yearIndex);
+        // console.log(data[i][yearIndex])
+        yearData[yearIndex] = data[i][yearIndex];
+      };
+      console.log(yearData)
+    };
+  };
+}
+
+
 // Load all 4 data files so we can utilze
 d3.queue()
 .defer(d3.csv, "data/imf_lending_borrowing_data_groups.csv")
@@ -9,10 +36,17 @@ d3.queue()
         console.error('Oh dear, something went wrong: ' + error);
     }
     else {
-        // doStuff(file1, file2);
-        console.log(lendingBorrowingDataGroups);
-        console.log(lendingBorrowingData);
-        console.log(expenditureData);
-        console.log(revenueData);
+        console.log("imf_lending_borrowing_data_groups.csv");
+        console.log('----------------------------------------------------');
+        outputData(lendingBorrowingDataGroups);
+        console.log("imf_lending_borrowing_data.csv");
+        console.log('----------------------------------------------------');
+        outputData(lendingBorrowingData);
+        console.log("Expenditure_Data_IMF.csvv");
+        console.log('----------------------------------------------------');
+        outputData(expenditureData);
+        console.log("Revenue_Data_IMF.csv");
+        console.log('----------------------------------------------------');
+        outputData(revenueData);
     }
 });
