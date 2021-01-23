33aaf668c2663ac306ec7e58e0da16634a3a75f2
Sirine <sirine.khelifi1@outlook.com>
2021-01-23 15:00:42 -0500

    merged and cleaned the final dataset for bubble chart

--- static/data.py
+++ static/data.py
@@ -1,8 +1,19 @@
-import panda as pd
+import pandas as pd
 import os
 import csv
+import pyMongo
 
 
 expenditure = pd.read_csv("data/Expenditure_Data_IMF.csv")
+expenditure = expenditure.dropna(axis=0)
+expenditure_df = pd.DataFrame(expenditure)
+#print(expenditure_df)
 revenue = pd.read_csv("data/Revenue_Data_IMF.csv")
-merged = expenditure.merge(revenue, on="")
+revenue= revenue.dropna(axis=0)
+revenue_df=pd.DataFrame(revenue)
+merged = expenditure_df.merge(revenue_df, on="Country")
+merged.to_csv("data/Rev_Exp_output.csv", index=False)
+#print(merged)
+merged_years = merged[["2017_x", "2018_x", "2019_x", "2020_x", "2017_y","2018_y", "2019_y","2020_y"]]
+#print(merged_years)
+merged_years.to_csv("data/Rev_Exp_years_output.csv", index=False)
