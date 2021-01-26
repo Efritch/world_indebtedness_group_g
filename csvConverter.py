1575762e6c0fac5ff659e9433fc8947c47717620
Efritch <70618234+Efritch@users.noreply.github.com>
2021-01-25 22:11:41 -0500

    finished code for uploading lend_borrow file

--- csvConverter.py
+++ csvConverter.py
@@ -10,29 +10,22 @@ uri = f"mongodb+srv://web_user:Uj3z9lLuFWfxoOfZ@projectcluster.2m1ao.mongodb.net
 mongo = MongoClient(uri)
 
 
-# Defining the database collections
+# Defining the database collection
 lend_borrow_col = mongo.db.lend_borrow
-print(lend_borrow_col)
-# lend_borrow_summary_col = db["lend_borrow_summary"]
-# expenditure_data_col = db["expenditures"]
-# revenue_data_col = db["revenues"]
+# print(lend_borrow_col)
+
 
 # Reading in the .csv files
 file_path_1 = os.path.join("data","imf_lending_borrowing_data_transpose.csv")
-# file_path_2 = os.path.join("world_indebtedness_group_g","data","imf_lending_borrowing_data_groups.csv")
-# file_path_3 = os.path.join("world_indebtedness_group_g","data","Expenditure_Data_IMF.csv")
-# file_path_4 = os.path.join("world_indebtedness_group_g","data","Revenue_Data_IMF.csv")
-
 lend_borrow = pd.read_csv(file_path_1, encoding="utf-8")
 lend_borrow["Year"].astype("str", copy=False)
-# lend_borrow_summary = pd.read.csv(file_path_2)
-# expenditures = pd.read.csv(file_path_3)
-# revenues = pd.read.csv(file_path_4)
 
-# Converting .csv files to JSON objects
-# lend_borrow.to_json(r'data/lend_borrow_data.json', orient='records', lines=True)
-#lend_borrow_summary.to_json(r'world_indebtedness_group_g\data/lend_borrow_group_data.json')
-#expenditures.to_json(r'world_indebtedness_group_g\data/expenditure_data.json')
-#revenues.to_json(r'world_indebtedness_group_g\data/revenue_data.json')
-#print(lend_borrow.to_dict("records"))
-lend_borrow_col.update({}, lend_borrow.to_dict("records"), upsert=True)
+
+# Loading the .csv file to MongoDB.
+# print(lend_borrow.to_dict("records")[0])
+for record in lend_borrow.to_dict("records"):
+    new_record = {}
+    for key, value in record.items():
+        new_record[key.replace('.','')] = value
+    lend_borrow_col.insert_one(new_record)
+
