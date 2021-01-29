3fedd4a9d7b19326ca3178b18cae7380b9c99da7
Beverly Thompson <blt1339@gmail.com>
2021-01-29 09:30:16 -0500

    Add labels to groups display.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 
 from flask import Flask, render_template, jsonify,request
 import pymongo
-
+import astropy.config.configuration
+from config import password
 #################################################
 # Flask Setup
 #################################################
@@ -22,17 +23,17 @@ def bubble():
     return render_template('bubble.html')
 
 
-@app.route('/db_data', methods=['GET'])
-def database_data():
-    collection_name = request.args.get('col',default='rev_exp',type=str)
-    # Connecting to the database
-    db_name="world_indebtedness"
-    uri=f"mongodb://web_user:Uj3z9lLuFWfxoOfZ@projectcluster.2m1ao.mongodb.net/{db_name}?retryWrites=true&w=majority"
-    mongo=pymongo.MongoClient(uri)
-    result = []
-    for doc in mongo.db[collection_name].find():
-        result.append(doc)
-    return jsonify(result)
+# @app.route('/db_data', methods=['GET'])
+# def database_data():
+#     collection_name = request.args.get('col',default='rev_exp',type=str)
+#     # Connecting to the database
+#     db_name="world_indebtedness"
+#     uri = f"mongodb://web_user:{password}Z@projectcluster-shard-00-00.2m1ao.mongodb.net:27017,projectcluster-shard-00-01.2m1ao.mongodb.net:27017,projectcluster-shard-00-02.2m1ao.mongodb.net:27017/world_indebtedness?ssl=true&replicaSet=atlas-12bec0-shard-0&authSource=admin&retryWrites=true&w=majority"
+#     mongo=pymongo.MongoClient(uri)
+#     result = []
+#     for doc in mongo.db[collection_name].find():
+#         result.append(doc)
+#     return result
 
 
 if __name__ == '__main__':
