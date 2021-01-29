3735f2b470be1a97314fe73e5c8e4e9803a4c9ce
Sirine <sirine.khelifi1@outlook.com>
2021-01-28 23:03:52 -0500

    working on scatter plot

--- static/js/logic.js
+++ static/js/logic.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
 // }
 
 
-//Load default 2003 data
+//Load default 2020 data
 currentYear = "2020";
 var yearSwitcher = new yearSwitcherConstructor(currentYear);
 //console.log(yearSwitcher);
@@ -200,7 +200,7 @@ function loadScatterplot() {
         console.log(d.Expenditure)
         return xScale(+d.Expenditure) } )
       .attr("cy", function (d) { return yScale(+d.Revenue) } )
-      .attr("r", 4)
+      .attr("r", 6)
       .attr("stroke", "black")
       .style("fill", "blue")
       //.style("fill", myColor(this))
@@ -266,149 +266,4 @@ function loadScatterplot() {
 }
 
 loadScatterplot();
-
-d3.csv("data/Revenue_Expenditure_data.csv", function(dataInfo) {
-
-  dataInfo.forEach(function (data){
-
-  // Add X axis
-  var xScale = d3.scaleLinear()
-    .domain(d3.extent(dataInfo, d =>d.Expenditure))
-    .range([0, chartWidth]);
-  svg.append("g")
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(d3.axisBottom(xScale))
-    .append("text")
-    .text("Expenditure")
-    .attr("transform", `translate(${chartWidth/2}, 30)`)
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "30px")
-    .attr("color", "black");
-
-
-  // Add Y axis
-  var yScale = d3.scaleLinear()
-    .domain(d3.extent(dataInfo, d =>d.Revenue))
-    .range([chartHeight, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(yScale))
-    .append("text")
-    .text("Revenue")
-    .attr("transform", `translate(${chartHeight/2}, 10)`)
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "30px")
-    .attr("color", "black");
-
-  // Add a scale for bubble size
-  // var z = d3.scaleLinear()
-  //   .domain([200000, 1310000000])
-  //   .range([ 4, 40]);
-
-  // Add a scale for bubble color
-  // var myColor = d3.scaleOrdinal()
-  //   .domain(["Country"])
-  //   .range(d3.schemeSet2);
-
-  // -1- Create a tooltip div that is hidden by default:
-  var tooltip = d3.select("#my-dataviz")
-      .append("div")
-      .style("opacity", 0)
-      .attr("class", "tooltip")
-      .style("background-color", "black")
-      .style("border-radius", "5px")
-      .style("padding", "10px")
-      .style("color", "white")
-
-  // -2- Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
-  var showTooltip = function(d) {
-    tooltip
-      .transition()
-      .duration(200)
-    tooltip
-      .style("opacity", 1)
-      .html("Country: " + d.country)
-      .style("left", (d3.mouse(this)[0]+30) + "px")
-      .style("top", (d3.mouse(this)[1]+30) + "px")
-  }
-  var moveTooltip = function(d) {
-    tooltip
-      .style("left", (d3.mouse(this)[0]+30) + "px")
-      .style("top", (d3.mouse(this)[1]+30) + "px")
-  }
-  var hideTooltip = function(d) {
-    tooltip
-      .transition()
-      .duration(200)
-      .style("opacity", 0)
-  }
-
-  var myColor = function(value) {
-    if (value = "") {  
-      fill = 'grey';
-  } else if ( value < 15) {  
-      fill = 'grey';
-  } else if (value => 15 && value < 30) {  
-      fill = 'yellow';
-  } else if (value => 30 && value < 40) {  
-      fill = 'orange';
-  } else if (value =>40 && value < 50) {  
-      fill = 'red';
-  }
-  else if (value =>50) {  
-    fill = 'purple';
-  }
-  }
-
-  // Add dots
-  svg.append('g')
-    .selectAll("dot")
-    .data(dataInfo)
-    .enter()
-    .append("circle")
-
-      .attr("class", "bubbles")
-      .attr("cx", function (d) { return x(d.gdpPercap); } )
-      .attr("cy", function (d) { return y(d.lifeExp); } )
-      .attr("r", function (d) { return z(d.pop); } )
-      .style("fill", function (d) { return myColor(d.continent); } )
-    // -3- Trigger the functions
-    .on("mouseover", showTooltip )
-    .on("mousemove", moveTooltip )
-    .on("mouseleave", hideTooltip )
-
-  })
-// var someJavaScriptVar = '{{ db_data[0]}}';
-
-// console.log(someJavaScriptVar);
-
-    .attr("class", "bubbles")
-    .attr("cx", function (d) { return xScale(d.Expenditure.round(2)); } )
-    .attr("cy", function (d) { return yScale(d.Revenue.round(2)); } )
-    .attr("r", function (d) { return Math.log(d.Expenditure.round(2));})
-    .style("fill", myColor(this))
-    .append("text")
-    .text(function(d) {
-      return `${d.Country}`;
-    })
-    .attr("dx", function(d) {           
-        return xScale(d.Expenditure);
-    })
-    .attr("text-anchor", "middle")
-    .attr("dy", function(d) {
-        return yScale(d.Revenue);
-    })
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "10px")
-    .attr("fill", "red")
-
-  // -3- Trigger the functions
-  .on("mouseover", showTooltip )
-  .on("mousemove", moveTooltip )
-  .on("mouseleave", hideTooltip )
-
-  })
-});
-
-=======
-// console.log(someJavaScriptVar);
 
